d286dab25cec80f333eb5d46eb2a2ea5bcda3782
Solobolt <solobolt@gmail.com>
2015-11-08 22:58:13 +1000

    Implimented Player movement and player aiming

---
+++ Aurora/Assets/Scripts/Player/PlayerAiming.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAiming : MonoBehaviour {
+
+    private int playerNumb;
+
+    private Transform myTransform;
+
+    //String variables
+    private string mouseX;
+    private string mouseY;
+
+    //Aiming Values
+    float aimingX;
+    float aimingY;
+
+    // Use this for initialization
+    void Start () {
+        //sets the players transform
+        myTransform = this.transform;
+
+        //Finds the player number from its parent
+        playerNumb = gameObject.GetComponentInParent<PlayerMovement>().playerNumb;
+
+        //Sets strong variables
+        mouseX = "P" + playerNumb + "_MouseX";
+        mouseY = "P" + playerNumb + "_MouseY";
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Quaternion currentAngle = myTransform.rotation;
+
+        // Handles aiming of player
+        if (Input.GetAxis(mouseX) != 0)
+        {
+            aimingX = -Input.GetAxis(mouseX);
+        }
+        else aimingX = 0;
+
+        if (Input.GetAxis(mouseY) != 0)
+        {
+            aimingY = -Input.GetAxis(mouseY);
+        }
+        else aimingY = 0;
+
+        //Culculates the desired angle of the Weapon Pivot
+        float angle = (Mathf.Atan2(aimingX, aimingY) * Mathf.Rad2Deg);
+
+        currentAngle = Quaternion.Euler(0,angle,0);
+
+        //Sets pivot angle
+        myTransform.rotation = currentAngle;
+    }
+}
---
+++ Aurora/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int playerNumb = 1;
+
+    //Holds movement values
+    private Transform myTransform;
+    private int moveSpeed = 10;
+
+    //Hold strings for the controller
+    private string horizontal;
+    private string vertical;
+    //private string fire1;
+    //private string fire2;
+    
+
+	// Use this for initialization
+	void Start () {
+        myTransform = this.transform;
+        SetStrings();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    //Sets strings depeing on the player numb
+    private void SetStrings()
+    {
+        //Base string { = "P" + playerNumb + "_"}
+        horizontal = "P" + playerNumb + "_Horizontal";
+        vertical = "P" + playerNumb + "_Vertical";
+       // fire1 = "P" + playerNumb + "_Fire1";
+       // fire2 = "P" + playerNumb + "_Fire2";
+    }
+
+    //Handles movement
+    private void Movement()
+    {
+        //Place holder for postion edits
+        Vector3 currentPos = myTransform.position;
+
+        //Moves the player alongg the X Axis
+        if (Input.GetAxis(horizontal) != 0)
+        {
+            currentPos.x += moveSpeed * Time.deltaTime * Input.GetAxis(horizontal);
+        }
+
+        //moves the player along the Y axis
+        if (Input.GetAxis(vertical) != 0)
+        {
+            currentPos.y += moveSpeed * Time.deltaTime * Input.GetAxis(vertical);
+        }
+
+        //sets the new postion
+        myTransform.position = currentPos;
+    }
+}
