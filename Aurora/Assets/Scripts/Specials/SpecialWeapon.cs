d5d72bf03ffac06eb96e635c16b115d101cf18e7
Solobolt <solobolt@gmail.com>
2015-11-10 11:01:05 +1000

    Created blank scripts for special abilities

---
+++ Aurora/Assets/Scripts/Specials/SpecialWeapon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialWeapon : MonoBehaviour {
+
+	private Transform myTransform;
+
+	private string fire2;
+
+	private int playerNumb;
+
+	// Use this for initialization
+	void Start () {
+		playerNumb = GetComponentInParent<PlayerAiming>().playerNumb;
+		myTransform = this.transform;
+		fire2 = "P" + playerNumb + "_Fire2";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Check for inputs
+	private void CheckInputs()
+	{
+		
+	}
+}
