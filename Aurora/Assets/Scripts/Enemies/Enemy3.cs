85d8fd0d71e78fc247d01ec066fdd55972ea2689
shabbyhalfling6 <bradycjohnston@hotmail.com>
2015-11-13 01:14:33 +1100

    Fixed Things

--- Aurora/Assets/Scripts/Enemies/Enemy3.cs
+++ Aurora/Assets/Scripts/Enemies/Enemy3.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class Enemy3 : Enemy {
 
-    int unitHealth = 50;
-    int unitSpeed = 2;
+    int unitHealth = 20;
+    int unitSpeed = 5;
     float fireTime = 10;
-    float fireRate = 1.5f;
+    float fireRate = 1.8f;
 
     public override void SetUnitValues()
     {
         health = unitHealth;
         moveSpeed = unitSpeed;
+
+		//Activate the player tracking system
+		track = true;
     }
 
     //Creates and fires enemy bullets in the direction that the enemy is facing
