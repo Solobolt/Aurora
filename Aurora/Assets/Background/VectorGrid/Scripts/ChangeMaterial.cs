48234b0214ac192f2b52c18a3afb9766368b09b3
CarlosMeloStuff <carlosmelo1991@gmail.com>
2015-11-14 18:17:08 +1000

    Some advances on the Aurora, I got the materials changing, now I need to rescale the textures and tweak the delay

---
+++ Aurora/Assets/Background/VectorGrid/Scripts/ChangeMaterial.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMaterial : MonoBehaviour 
+{
+	public Material[] materials;
+	public int delay;
+	private int index;
+	public Renderer renderer;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		index = 0;
+		renderer = GetComponent<Renderer> ();
+		//renderer.enabled=true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		if (Input.GetKeyUp (KeyCode.M)) 
+//		{
+//			this.renderer.material= materials [index];
+//			index = (index + 1) % materials.Length;
+//		}
+		StartCoroutine (ChangingMaterials ());
+	}
+
+	IEnumerator ChangingMaterials()
+	{
+		if (index == 0) 
+		{
+			yield return new WaitForSeconds (delay);
+			this.renderer.material = materials [index];
+			index = 1;
+			yield return new WaitForSeconds (delay);
+			this.renderer.material = materials [index];
+			index = 2;
+			yield return new WaitForSeconds (delay);
+			index=0;
+		}
+
+		//index = (index + 1) % materials.Length;
+
+	}
+
+}
