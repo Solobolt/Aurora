7925280ec409732563b04c31cc4f6a0ec5b7435a
Solobolt <solobolt@gmail.com>
2015-11-23 21:09:56 +1000

    Implimented Enemy weapon swap depending on miniboss health

--- Aurora/Assets/Scripts/Enemies/Miniboss.cs
+++ Aurora/Assets/Scripts/Enemies/Miniboss.cs
@@ -5,20 +5,33 @@ using System.Collections;
 public class Miniboss : MonoBehaviour {
 
     //sets the miniboss's health
-    private int health = 1000;
+    private int health = 10000;
     private int TotalHealth = 1000;
     private int damage = 10;
     public Slider healthBarSlider;
 
+    //Weapon GameObjects
+    public GameObject weapon1;
+    public GameObject weapon2;
+    public GameObject weapon3;
+
     // Use this for initialization
     void Start () {
-	
-	}
+        SetWeapons(true,false,false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        healthWeaponCheck();
 	}
+    
+    //activates wepons
+    void SetWeapons(bool _weapon1, bool _weapon2,bool _weapon3)
+    {
+        weapon1.SetActive(_weapon1);
+        weapon2.SetActive(_weapon2);
+        weapon3.SetActive(_weapon3);
+    }
 
     // Removes health if the player is hit by a laser
     void OnTriggerEnter(Collider otherObject)
@@ -29,11 +42,26 @@ public class Miniboss : MonoBehaviour {
         }
     }
 
+    //Changes weapon depedning on the health of the boss
+    void healthWeaponCheck()
+    {
+        //swaps the enemy weapon 
+        if (health <= TotalHealth * 0.6f && health > TotalHealth * 0.3)
+        {
+            SetWeapons(false,true,false);
+        }
+
+        if (health <= TotalHealth * 0.3f)
+        {
+            SetWeapons(false, false, true);
+        }
+    }
+
     //Removes health
     void RemoveHealth()
     {
         health -= damage;
-        healthBarSlider.value = healthBarSlider.value - 0.01f;
+        healthBarSlider.value = healthBarSlider.value - 0.001f;
 
         if (health <= 0)
         {
