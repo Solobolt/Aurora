ee536d0db5e12636915d3062fbab2dfb823f99ed
Solobolt <solobolt@gmail.com>
2015-11-25 22:02:54 +1000

    Started ship selection using player prefs

---
+++ Aurora/Assets/Scripts/UI/ShipSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Sets the player 1st players ship type
+    public void P1SetShipType(int ship)
+    {
+            PlayerPrefs.SetInt("P1_SHIPTYPE", ship);
+        print("Setting player pref 1");
+    }
+
+    //Sets the player 1st players ship type
+    public void P2SetShipType(int ship)
+    {
+        PlayerPrefs.SetInt("P2_SHIPTYPE", ship);
+        print("Setting player pref 2");
+    }
+}
